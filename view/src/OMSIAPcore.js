b91a44d4bacfe2a019dc4dbca5af17ba3b057294
markie311 <markiebeloy@gmail.com>
2025-06-05 18:00:00 +0800

    Server issues being fixed

--- view/src/OMSIAPcore.js
+++ view/src/OMSIAPcore.js
@@ -295,6 +295,7 @@ function OMSIAPCoreContent() {
       if (response.data && response.data.products) {
 
         alloftheproductscb(response.data.products);
+        alert(JSON.stringify(response.data.products))
         alert("Reached")
 
       } else {
