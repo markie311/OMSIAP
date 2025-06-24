07b0daa68db7679aed0a5e99248e37b29d8c0991
markie311 <markiebeloy@gmail.com>
2025-06-25 00:22:52 +0800

    New product images added

--- view/src/components/market/market-component.js
+++ view/src/components/market/market-component.js
@@ -374,7 +374,6 @@ const prevImage = () => {
   return (
     <div className="market-container">
 
-
       {/* Marketing Carousel Header */}
       <div className="market-carousel-container">
         <div className="market-carousel-track" ref={carouselRef}>
