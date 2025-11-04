c25dbe019888bc28d285b4612bc277dd0dd37e1b
markie311 <markiebeloy@gmail.com>
2025-11-04 19:53:22 +0800

    New product added

--- view/src/components/lib/axiosutil.js
+++ view/src/components/lib/axiosutil.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosCreatedInstace = axios.create({baseURL: 'http://localhost:4000/'});
-//const axiosCreatedInstace = axios.create({baseURL: 'https://omsiapwebservice.onrender.com/'});
+//const axiosCreatedInstace = axios.create({baseURL: 'http://localhost:4000/'});
+const axiosCreatedInstace = axios.create({baseURL: 'https://omsiapwebservice.onrender.com/'});
                                     
 export default axiosCreatedInstace;
